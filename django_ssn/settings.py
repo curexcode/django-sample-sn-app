56a38919c8206f7ffe4940db0ff39b8becbd086a
curex <47378577+curexcode@users.noreply.github.com>
2021-04-07 13:48:53 +0530

    Add PendingConnection model, alter Comment model.

--- django_ssn/settings.py
+++ django_ssn/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'account',
     'api',
     'knox',
+    'feed',
     'phonenumber_field',
     'rest_framework',
     'django.contrib.admin',
--- feed/models.py
+++ feed/models.py
@@ -15,32 +15,48 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.OneToOneField(Account, null=True, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
     image = models.ImageField(null = True, blank = True, upload_to='images/')
     text = models.CharField(max_length=512)
-    likes = models.PositiveIntegerField()
+    likes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return str(self.text) + ' | ' + str(self.post)
+
+class PendingConnection(models.Model):
+    sender = models.PositiveIntegerField()
+    receiver = models.ForeignKey(Account, related_name="receiver", null=False, on_delete=models.CASCADE)
+
+    # @classmethod
+    # def add_pending(cls, from, to):
+    #     connection, created = cls.objects.get_or_create(from=)
+
+
 
 class Connection (models.Model):
     user = models.ManyToManyField(Account) 
     current_user = models.ForeignKey(Account, related_name='owner', null=True, on_delete=models.CASCADE)
 
     @classmethod
-    def add_connection(cls, current_user, new_connection):
-        current_user_id = Account.objects.get(pk=current_user)
-        connection, created = cls.objects.get_or_create(current_user=current_user_id)
-        connection.user.add(new_connection)
+    def add_friend(cls, current_user_id, new_friend_id):
+        current_user = Account.objects.get(pk=current_user_id)
+        new_friend  = Account.objects.get(pk=new_friend_id)
+        # Add the new_friend as friend of current_user
+        connection, created = cls.objects.get_or_create(current_user=current_user)
+        connection.user.add(new_friend)
+        # Add the current user as friend of new_friend
+        connection, created = cls.objects.get_or_create(current_user=new_friend)
+        connection.user.add(current_user)
 
     @classmethod
-    def remove_connection(cls, current_user, old_connection):
+    def remove_friend(cls, current_user, old_connection):
         current_user_id = Account.objects.get(pk=current_user)
         connection, created = cls.objects.get_or_create(current_user=current_user_id)
         connection.user.remove(old_connection)
 
     @classmethod    
-    def get_connection(cls, current_user):
+    def get_friends(cls, current_user):
         '''
         Gives you list of all connections of given user ID.
         '''
